a9918378ffe862542344865c91d2341c6655110b
anttilemp <121185188+anttilemp@users.noreply.github.com>
2022-12-21 18:57:11 +0200

    Update PairwiseCorrelationImage.py
    Creates correlation matrix.

--- PairwiseCorrelationImage.py
+++ PairwiseCorrelationImage.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #read in data
-df = pd.read_excel('RxnScreenDesc.xlsx', 'Desc')
+df = pd.read_excel('ModelExcel.xlsx', 'Desc')
 df.drop(columns='Ligand_name', inplace=True)
 
 #df2 = pd.read_excel('Descriptorsfromlasso.xlsx')
@@ -21,13 +21,19 @@ numerical_vars = list(data_x.select_dtypes(include=numerics).columns)
 
 df2 = df[numerical_vars]
 
+#pairwise correlations
+
 correlations = pg.pairwise_corr(df2, columns='AP')
+
+#select top 75 or desired amount
 
 best = correlations.nlargest(75, "power")
 a = best['Y']
 matrix = a.values.tolist()
 
 final = df[matrix].corr()
+
+# you should change these to edit image details
 
 plt.figure(figsize=(100, 80))
 cmap = sns.diverging_palette(250, 15, s=75, l=40, n=9, center="light", as_cmap=True)
