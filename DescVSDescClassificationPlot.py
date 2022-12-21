22850f6e48785e1b49108b2411eb79ee9f5b7294
anttilemp <121185188+anttilemp@users.noreply.github.com>
2022-12-21 21:01:59 +0200

    Update and rename DescVSDesc.py to DescVSDescClassificationPlot.py

--- DescVSDescClassificationPlot.py
+++ DescVSDescClassificationPlot.py
@@ -5,10 +5,14 @@ from sklearn.svm import SVR
 
 
 #read in data
-df = pd.read_excel('Descriptors_omat_laaja.xlsx', 'Sheet1')
+df = pd.read_excel('ModelExcel.xlsx', 'Desc')
 #df.drop(columns='Ligand_name', inplace=True)
 
+# read in screening data, plotted with red. Could be edited to not plot screening data...
+
 df2 = pd.read_excel('First_Descriptors_ready_v2.xlsx', 'PScreen')
+
+#Descriptors to plot
 
 n = df['Ligand'].tolist()
 
@@ -16,11 +20,7 @@ x = df['Ni_VBur_Boltz']
 
 y = df["Ni_electrophilicity_Boltz"]
 
-#Ni_electrophilicity_Boltz
-#Ni_VBur_Boltz
-#p_int_c_average_ensemble_max
-#C_VBur_average_Boltz
-#Ni_electrophilicity_Boltz
+#Descriptors to plot
 
 n2 = df2['Ligand_name'].tolist()
 
@@ -28,10 +28,13 @@ x2 = df2['Ni_VBur_Boltz']
 
 y2 = df2["Ni_electrophilicity_Boltz"]
 
+#figure details
+
 plt.figure(figsize=(20,20))
 plt.scatter(x, y, s=5, color="blue", label="Ligand")
 plt.scatter(x2, y2, s=5, color="red", label="Screening Ligand")
 
+# dot name assignation 
 
 for i, label in enumerate(n):
     plt.text(x[i], y[i], s=label)
@@ -39,7 +42,8 @@ for i, label in enumerate(n):
 
 for i, label in enumerate(n2):
     plt.text(x2[i], y2[i], s=label)
-
+    
+#labels
 
 plt.xlabel("Ni_VBur_Boltz")
 plt.ylabel("Ni_electrophilicity_Boltz")
